3e70baf67983c0c8be7b33200b0dedabf565fe9f
Lucashsmello <lucashsmello@gmail.com>
2020-04-06 10:08:17 -0300

    Setup config

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='siamese-triplet',
+    version='0.5',
+    description='Fork from adambielski/siamese-triplet',
+    url='https://github.com/Lucashsmello/siamese-triplet',
+    author='Lucas Mello',
+    author_email='lucashsmello@gmail.com',
+    license='public',
+    packages=["siamese_triplet"],
+    install_requires=[
+        "torch",
+        "numpy"
+    ],
+    zip_safe=False)
